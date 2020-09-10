d92f7fa699b1f51b05d5ecca10e38e7da97d038e
teguh02 <teguhrijanandi02@gmail.com>
2020-09-10 15:47:18 +0700

    first

---
+++ App/core.php
@@ -0,0 +1,27 @@
+<?php 
+
+/**
+ * Namespace merupakan kumpulan entities (class, object, function) 
+ * yang dikelompokkan dalam satu nama. 
+ */
+
+namespace App\core;
+
+/**
+ * Class adalah bisa dikatakan seperti keluarga
+ * jadi didalam class core akan ada beberapa anggota keluarga
+ */
+
+class core {
+
+    /**
+     * Method ini adalah method yang akan dipanggil
+     * supaya aplikasi kita berjalan
+     */
+
+    public function start()
+    {
+        # code...
+    }
+
+}
